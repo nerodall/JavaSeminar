1cc02509b0ae1a3de1b935e317a381cf34969eee
алексей нерода <neroda.all@mailru>
2023-06-25 17:09:09 +0300

    HW_ semonar6

---
+++ HW_Seminar6/src/App.java
@@ -0,0 +1,9 @@
+import Store.Laptop;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Laptop oe = new Laptop("Lenovo", "x61", 8, 256, null, null, 0);
+    }
+
+   
+}
---
+++ HW_Seminar6/src/Store/Laptop.java
@@ -0,0 +1,29 @@
+package Store;
+
+public class Laptop {
+    public String brand;
+    public String model;
+    public byte ram;
+    public short memory;
+    public String operationSystem;
+    public String color;
+    public int id;
+
+    public Laptop(String brand, String model, byte ram, short memory, String operationSystem, String color, int id) {
+        this.brand = brand;
+        this.model = model;
+        this.ram = ram;
+        this.memory = memory;
+        this.operationSystem = operationSystem;
+        this.color = color;
+        this.id = id;
+    }
+
+
+
+
+
+
+    
+
+}
