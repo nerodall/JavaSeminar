f6a283fb851e7ff96cecf609aaaf947a8a03539d
Alexey Neroda <neroda.all@mail.ru>
2023-06-25 23:35:42 +0300

    HW_seminar6 done

--- HW_Seminar6/src/App.java
+++ HW_Seminar6/src/App.java
@@ -1,9 +1,41 @@
+import java.util.HashSet;
+
 import Store.Laptop;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Laptop oe = new Laptop("Lenovo", "x61", 8, 256, null, null, 0);
+
+        // добавляем экземпляры класса
+        Laptop firstL = new Laptop("Lenovo", "x61", 8, 256, null, "black", 0);
+        Laptop secondL = new Laptop("MSI", "x500", 32, 1024, null, "red", 1);
+        Laptop thirdL = new Laptop("HP", "1298", 4, 128, null, "white", 2);
+        Laptop fourthL = new Laptop("Lenovo", "x61", 8, 256, null, "black", 3);
+
+        // создаем набор и добавляем экземпляры
+        HashSet<Laptop> storeCell = new HashSet<>();
+        storeCell.add(firstL);
+        storeCell.add(secondL);
+        storeCell.add(secondL);
+        storeCell.add(thirdL);
+        storeCell.add(fourthL);
+
+        // Поиск элемента в наборе и вывод при нахождении совпадений
+        Store.Laptop.searchLaptop(storeCell, "HP");
+
+        // Печать экземпляра класса
+        System.out.println(firstL);
+
+        // Сравнение нескольких элементов, в расчет не берется id
+        System.out.println(firstL.equals(secondL));
+        System.out.println(firstL.equals(fourthL));
+
+        //Печать HashCode элементов в наборе
+        System.out.println();
+        Store.Laptop.prntHash(storeCell);
+        //Печать всего множества
+        System.out.println();
+        System.out.println(storeCell);
+
     }
 
-   
 }
--- HW_Seminar6/src/Store/Laptop.java
+++ HW_Seminar6/src/Store/Laptop.java
@@ -1,15 +1,18 @@
 package Store;
 
+import java.util.HashSet;
+import java.util.Objects;
+
 public class Laptop {
     public String brand;
     public String model;
-    public byte ram;
-    public short memory;
+    public long ram;
+    public long memory;
     public String operationSystem;
     public String color;
     public int id;
 
-    public Laptop(String brand, String model, byte ram, short memory, String operationSystem, String color, int id) {
+    public Laptop(String brand, String model, long ram, long memory, String operationSystem, String color, int id) {
         this.brand = brand;
         this.model = model;
         this.ram = ram;
@@ -19,11 +22,67 @@ public class Laptop {
         this.id = id;
     }
 
+    public static void searchLaptop(HashSet<Laptop> set, String serchingObject) {
 
+        for (Object item : set) {
+            if (item.toString().contains(serchingObject)) {
+                System.out.println("Обнаружено совпадение");
+                System.out.println(item);
+            }
+        }
 
+    }
 
+    public static void prntHash(HashSet<Laptop> set) {
+        for (Object item: set){
+            System.out.println(item.hashCode());
+        }
+    }
 
+    @Override
+    public String toString() {
+        return "Laptop: \nbrand-" + brand + "\nmodel-" + model + "\nram-" + ram + "\nmemory-" + memory
+                + "\noperationSystem-" + operationSystem + "\ncolor-" + color + "\nid-" + id + "\n";
+    }
 
-    
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, model, ram, memory, operationSystem, color, id);
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Laptop other = (Laptop) obj;
+        if (brand == null) {
+            if (other.brand != null)
+                return false;
+        } else if (!brand.equals(other.brand))
+            return false;
+        if (model == null) {
+            if (other.model != null)
+                return false;
+        } else if (!model.equals(other.model))
+            return false;
+        if (ram != other.ram)
+            return false;
+        if (memory != other.memory)
+            return false;
+        if (operationSystem == null) {
+            if (other.operationSystem != null)
+                return false;
+        } else if (!operationSystem.equals(other.operationSystem))
+            return false;
+        if (color == null) {
+            if (other.color != null)
+                return false;
+        } else if (!color.equals(other.color))
+            return false;
+        return true;
+    }
 }
